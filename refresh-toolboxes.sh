f4506ca71184689e7889432db69515ac5462b257
Donato Capitella <donato.capitella@withsecure.com>
2025-09-16 12:34:38 +0100

    remove rocm6.4.2 from refresh script

--- refresh-toolboxes.sh
+++ refresh-toolboxes.sh
@@ -7,8 +7,6 @@ declare -A TOOLBOXES
 
 TOOLBOXES["llama-vulkan-amdvlk"]="docker.io/kyuz0/amd-strix-halo-toolboxes:vulkan-amdvlk --device /dev/dri --group-add video --security-opt seccomp=unconfined"
 TOOLBOXES["llama-vulkan-radv"]="docker.io/kyuz0/amd-strix-halo-toolboxes:vulkan-radv --device /dev/dri --group-add video --security-opt seccomp=unconfined"
-TOOLBOXES["llama-rocm-6.4.2"]="docker.io/kyuz0/amd-strix-halo-toolboxes:rocm-6.4.2 --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
-TOOLBOXES["llama-rocm-6.4.2-rocwmma"]="docker.io/kyuz0/amd-strix-halo-toolboxes:rocm-6.4.2-rocwmma --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
 TOOLBOXES["llama-rocm-6.4.3"]="docker.io/kyuz0/amd-strix-halo-toolboxes:rocm-6.4.3 --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
 TOOLBOXES["llama-rocm-6.4.3-rocwmma"]="docker.io/kyuz0/amd-strix-halo-toolboxes:rocm-6.4.3-rocwmma --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
 TOOLBOXES["llama-rocm-7rc"]="docker.io/kyuz0/amd-strix-halo-toolboxes:rocm-7rc --device /dev/dri --device /dev/kfd --group-add video --group-add render --group-add sudo --security-opt seccomp=unconfined"
@@ -19,7 +17,7 @@ function usage() {
   echo "Usage: $0 [all|toolbox-name1 toolbox-name2 ...]"
   echo "Available toolboxes:"
   for name in "${!TOOLBOXES[@]}"; do
-    echo "  - $name"
+    echo "  - $name"  
   done
   exit 1
 }
